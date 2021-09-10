f3c83d74d84f7d98546a3379ca66c1029c13bcba
alclopes <alclopes@gmail.com>
2021-09-10 07:21:05 -0300

    Finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import { MovieProps } from '../types';
+
+interface Props {
+  movies: MovieProps[];
+  title: string;
+}
+
+export function Content(props: Props) {
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {props.title}</span>
+        </span>
+      </header>
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value}/>
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
